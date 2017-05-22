a11e7e75f57f4c4f6e9f4fc7c0af7158186093b2
Andrew Clarke <andrewclarke982@gmail.com>
2017-05-22 16:17:35 +0100

    first commit

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'shotgun'
+require 'sinatra'
+
+get '/' do
+  "hello!"
+end
+
+
+get '/secret' do
+  "the meaning of life = pizza spy"
+end
+
+get '/secret2' do
+  "the actual meaning of life = 42"
+end
